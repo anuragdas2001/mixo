c6eb1ea2e54a26e1e4ce5975f87fc5c7832f78d8
anuragdas2001 <anuragdas12921@gmail.com>
2025-12-28 13:39:43 +0530

    added types for campaigns

---
+++ src/models/campaigns.ts
@@ -0,0 +1,12 @@
+type TStatus = "active" | "paused" | "completed";
+
+export interface ICampaigns {
+  id: string;
+  name: string;
+  brand_id: string;
+  status: TStatus;
+  budget: number;
+  daily_budget: number;
+  platforms: string;
+  created_at: Date;
+}
