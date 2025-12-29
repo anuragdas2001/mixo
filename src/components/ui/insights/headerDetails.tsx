60742c741c8c6ec755bcd1ff47a0384c662decf1
anuragdas2001 <anuragdas12921@gmail.com>
2025-12-29 14:51:46 +0530

    added a link for viewing streams data

--- src/components/ui/insights/headerDetails.tsx
+++ src/components/ui/insights/headerDetails.tsx
@@ -1,11 +1,12 @@
 import { BarChart3 } from "lucide-react";
+import Link from "next/link";
 
 // Component: Header Section
 const HeaderDetails = ({
   campaignId,
   timestamp,
 }: {
-  campaignId: string; 
+  campaignId: string;
   timestamp: string;
 }) => (
   <div className="bg-white rounded-2xl shadow-lg p-8 border border-slate-200">
@@ -21,9 +22,12 @@ const HeaderDetails = ({
         </div>
         <p className="text-slate-600 md:ml-14">Campaign ID: {campaignId}</p>
       </div>
-      <div className="text-right">
+      <div className="text-right flex flex-col gap-2">
         <p className="text-sm text-slate-500">Last Updated</p>
         <p className="text-sm font-medium text-slate-700">{timestamp}</p>
+        <Link href={`/campaigns/${campaignId}/insights/streams`} className="bg-green-400 h-10 px-4 py-2 rounded-2xl ">
+          View streams
+        </Link>
       </div>
     </div>
   </div>
