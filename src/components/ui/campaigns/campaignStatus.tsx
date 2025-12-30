02b48433627b3a90474e70a922940f9faac0badd
anuragdas2001 <anuragdas12921@gmail.com>
2025-12-30 11:47:35 +0530

    fixed some alignments

--- src/components/ui/campaigns/campaignStatus.tsx
+++ src/components/ui/campaigns/campaignStatus.tsx
@@ -2,24 +2,25 @@ import { Button } from "@/components/ui/button";
 import { formatDate, getStatusColor } from "@/lib/helper";
 import { ICampaignStatusProps } from "@/models/campaigns";
 import { useRouter } from "next/navigation";
-
+import { Badge } from "@/components/ui/badge";
 const CampaignStatus = ({ id, status, createdAt }: ICampaignStatusProps) => {
   const router = useRouter();
 
   return (
     <div className="bg-white rounded-2xl shadow-sm border p-6">
-      <h2 className="text-lg font-semibold mb-4">Status</h2>
+      <div className="flex">
+        <h2 className="text-lg font-semibold mb-4">Status</h2>
+        <Badge
+          className={`px-4 h-8 w-20 ml-auto rounded-full text-sm font-medium border ${getStatusColor(
+            status
+          )}`}
+        >
+          {status.charAt(0).toUpperCase() + status.slice(1)}
+        </Badge>
+      </div>
 
       <div className="flex justify-between items-center gap-3">
         <div className="flex flex-col gap-2">
-          <span
-            className={`px-4 py-2 rounded-full text-sm font-medium border ${getStatusColor(
-              status
-            )}`}
-          >
-            {status.charAt(0).toUpperCase() + status.slice(1)}
-          </span>
-
           <span className="text-sm text-slate-500">
             Created {formatDate(createdAt)}
           </span>
