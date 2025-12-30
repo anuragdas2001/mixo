7a921596f32e1ce3763a89ffd1cfbe918332db39
anuragdas2001 <anuragdas12921@gmail.com>
2025-12-30 11:47:57 +0530

    fixed text size and alignments

--- src/components/ui/insights/headerDetails.tsx
+++ src/components/ui/insights/headerDetails.tsx
@@ -18,11 +18,15 @@ const HeaderDetails = ({
             <div className="p-2 bg-gradient-to-br from-indigo-500 to-purple-600 rounded-xl">
               <BarChart3 className="w-6 h-6 text-white" />
             </div>
-            <h1 className="text-3xl font-bold text-slate-800">
-              Campaign Insights
-            </h1>
+            <div className="flex flex-col">
+              <h1 className="text-xs sm:text-sm md:text-xl lg:text-3xl font-bold text-slate-800">
+                Campaign Insights
+              </h1>
+              <p className="text-slate-600 text-xs">
+                Campaign ID: {campaignId}
+              </p>
+            </div>
           </div>
-          <p className="text-slate-600 md:ml-14">Campaign ID: {campaignId}</p>
         </div>
         <div className="text-right flex flex-col gap-2">
           <p className="text-sm text-slate-500">Last Updated</p>
@@ -32,9 +36,9 @@ const HeaderDetails = ({
             onClick={() =>
               router.push(`/campaigns/${campaignId}/insights/streams`)
             }
-            className=" h-10 px-4 py-2 rounded-2xl "
+            className=" h-10 w-32 rounded-2xl text-xs"
           >
-            View streams
+            Stream insights
           </Button>
         </div>
       </div>
